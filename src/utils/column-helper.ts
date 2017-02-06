7ee28eaff82ae86222469de03c25ea04e2421bd7
Andy Shin <andy.yoonyong@gmail.com>
2017-02-06 14:17:02 +0900

    [FIX]move camelCase to local function

--- src/utils/column-helper.ts
+++ src/utils/column-helper.ts
@@ -1,5 +1,42 @@
 import { DataTableColumnDirective } from '../components/columns';
-import { camelCase, deCamelCase } from '../utils';
+
+/**
+ * Converts strings from something to camel case
+ * http://stackoverflow.com/questions/10425287/convert-dash-separated-string-to-camelcase
+ * @param  {string} str
+ * @return {string} camel case string
+ */
+export function camelCaseLocal(str: string): string {
+  // Replace special characters with a space
+  str = str.replace(/[^a-zA-Z0-9 ]/g, ' ');
+  // put a space before an uppercase letter
+  str = str.replace(/([a-z](?=[A-Z]))/g, '$1 ');
+
+  // Lower case first character and some other stuff
+  str = str.replace(/([^a-zA-Z0-9 ])|^[0-9]+/g, '').trim().toLowerCase();
+
+  // uppercase characters preceded by a space or number
+  str = str.replace(/([ 0-9]+)([a-zA-Z])/g, function(a, b, c) {
+    return b.trim() + c.toUpperCase();
+  });
+
+  return str;
+}
+
+/**
+ * Converts strings from camel case to words
+ * http://stackoverflow.com/questions/7225407/convert-camelcasetext-to-camel-case-text
+ *
+ * @export
+ * @param {any} str
+ * @returns string
+ */
+export function deCamelCaseLocal(str: string): string {
+  return str
+    .replace(/([A-Z])/g, (match) => ` ${match}`)
+    .replace(/^./, (match) => match.toUpperCase());
+}
+
 
 /**
  * Sets the column defaults
@@ -18,12 +55,12 @@ export function setColumnDefaults(columns: any[]) {
 
     // translate name => prop
     if(!column.prop && column.name) {
-      column.prop = camelCase(column.name);
+      column.prop = camelCaseLocal(column.name);
     }
 
     // format props if no name passed
     if(column.prop && !column.name) {
-      column.name = deCamelCase(column.prop);
+      column.name = deCamelCaseLocal(column.prop);
     }
 
     if(!column.hasOwnProperty('resizeable')) {
